af7bbc517fc6d8134801ed36d7dd15c7cadf52f0
André Tashiro <andre_seichi@hotmail.com>
2021-12-20 15:28:48 -0300

    componente de GoTop

---
+++ src/components/GoTop/GoTop.stories.jsx
@@ -0,0 +1,103 @@
+import { GoTop } from '.';
+export default {
+  title: 'GoTop',
+  component: GoTop,
+};
+
+export const Template = (args) => {
+  return (
+    <div style={{ height: '400vh' }}>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero autem
+        ullam placeat
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        voluptates sed cupiditate ratione? Accusantium magni deleniti molestiae.
+        Deleniti, quod temporibus quis nostrum eius repudiandae nam rerum libero
+        voluptatum illum!
+      </p>
+      <GoTop {...args} />
+    </div>
+  );
+};
