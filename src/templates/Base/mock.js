86063521f0777328212009c63501bb5181eeef31
André Tashiro <andre_seichi@hotmail.com>
2021-12-20 19:18:49 -0300

    template de Base

---
+++ src/templates/Base/mock.js
@@ -0,0 +1,10 @@
+import linksMock from '../../components/NavLinks/mock';
+
+export default {
+  links: linksMock,
+  logoData: {
+    text: 'logo',
+    link: '#',
+  },
+  footerHtml: '<p>Teste de Footer</p>',
+};
