94a199222a984add950934a72e4bf550001a08c6
André Tashiro <andre_seichi@hotmail.com>
2022-01-12 17:22:15 -0300

    renderizando os componentes desejados pela api

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,15 @@
-import { mockBase } from '../Base/mock';
-import { Base } from '../Base';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { mapData } from '../../api/map-data';
-import { useState } from 'react';
+
+import { Base } from '../Base';
+
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+import { GridTwoColumns } from '../../components/GridTwoColumns';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
 
 export function Home() {
   const [data, setData] = useState([]);
@@ -18,7 +22,6 @@ export function Home() {
         );
         const json = await data.json();
         const pageData = mapData(json);
-        console.log(pageData);
 
         setData(pageData[0]);
       } catch (error) {
@@ -37,5 +40,35 @@ export function Home() {
     return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumns key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
